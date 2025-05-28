014fda640aeff081aa8f852b293f9316adfa5b34
Cornichon <antoni.blanche@proton.me>
2025-05-28 12:12:05 +0200

    Bad file name

--- src/postProcessor.py
+++ src/postProcessor.py
@@ -10,7 +10,7 @@ def generate_init_vti(V0, psi_real_part, psi_imag_part,):
     @param psi_imag_part, imaginary part of the psi function
     """
     Psi_module = np.sqrt(np.power(psi_real_part, 2) + np.power(psi_imag_part, 2))
-    file_name = "vti/initial_state"
+    file_name  = "vti/initial_state"
     imageToVTK(file_name, pointData=
             {
                 'psi_module' : Psi_module.astype(np.float32).reshape((psi_real_part.shape[0], psi_real_part.shape[1], 1), order='C'),
@@ -30,7 +30,7 @@ def generate_vti(V0, psi_imag_part, psi_real_part, id):
     @id the id of the state, to differentiate the vti files
     """
     Psi_module = np.sqrt(np.power(psi_real_part, 2) + np.power(psi_imag_part, 2))
-    file_name = "vti/output_vti__%d" % (id)
+    file_name  = "vti/output_vti__%04d" % (id)
     imageToVTK(file_name, pointData=
             {
                 'psi_module' : Psi_module.astype(np.float32).reshape((psi_real_part.shape[0], psi_real_part.shape[1], 1), order='C'),
@@ -38,4 +38,3 @@ def generate_vti(V0, psi_imag_part, psi_real_part, id):
                 'psi_imag_part' : psi_imag_part.astype(np.float32).reshape((psi_imag_part.shape[0], psi_imag_part.shape[1], 1), order='C'),
                 'V0' : V0.astype(np.float32).reshape((V0.shape[0], V0.shape[1], 1), order='C')
             })
-
